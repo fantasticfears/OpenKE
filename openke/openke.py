a7c62c0daa589fc1b9964d77a8c8f4b0b20ebdd7
Erick Guan <fantasticfears@gmail.com>
2018-01-29 15:19:23 +0100

    don't convert to tensor

--- openke/openke.py
+++ openke/openke.py
@@ -42,9 +42,9 @@ def build_optimizer(optimizer, options):
 def prepare_batch(next_elements, session):
   el = [tf.transpose(n, perm=[1, 0]) for n in next_elements]
   el = reduce(lambda x,y: x+y, el)
-  tensor = tf.convert_to_tensor(el)
+  # tensor = tf.convert_to_tensor(el)
   # print(session.run(tf.Print(el, [el])))
-  return tensor
+  return el
 
 class Step(object):
   """A class takes a train config and execute a run."""
